ef02f79dcea0668d7de4fb1d54d51e5c8960e505
deftydev <devanshgupta79212346@gmail.com>
2023-03-23 10:18:16 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import find_packages,setup
 from typing import List
 
+
+automated="-e ."
 def get_requirements(file_path:str)->List[str]:
 
     requirements=[]
     with open(file_path) as f:
         requirements= f.readlines()
         requirements=[r.replace("\n"," ") for r in requirements]
+
+        if automated in requirements:
+            requirements.remove(automated)
+
+    return requirements
 
 
 
