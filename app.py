120228eb4f347a4d3aef29e23af8d537343e4a65
deftydev <devanshgupta79212346@gmail.com>
2023-03-26 16:57:38 +0530

    flask application

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
-app= Flask(__name__)
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
-@app.route("/",methods=['GET','POST'])
+
+
+application= Flask(__name__)
+app=application
+@app.route("/")
 def index():
-    return "Hello World"
+    return render_template('index.html')
 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.methods=='GET':
+        return render_template('home.html')
 
 if __name__ =="__main__":
     app.run(debug=True)
